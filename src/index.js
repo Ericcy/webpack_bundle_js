1e7b4108141fb6bd97675dbed8aa990f65e12c6d
leilangfeng <leilangfei@dongfangfuli.com>
2019-10-31 15:45:05 +0800

    监听history

--- src/index.js
+++ src/index.js
@@ -10,26 +10,30 @@ class DF_SDK_Collect {
     constructor(obj){
         this.version = '1.0.0';
         console.log('埋点的版本号：' + this.version);
-        this.project = ''; // 参数列表：VUE, REACT
-        this.router = null; // 只有SPA的情况下才有router对象
         this.sendUrl = 'http://static.dongfangfuli.com';
         this.commonUpData = {
-            userId: utils.cookie.getItem('mall_userId') || '',      // 用户id
-            sessionId: utils.cookie.getItem('sessionId') || '',                                     // 设备号
-            plantform: '0',                                               // 平台
-            pageUrl: location.href,                                      // 当前页面地址
-            pageName: document.title || '',                              // 当前页面的标题
-            pageFrom: document.referrer,                                 // 当前页面的来源
-            eventType: '',                                               // 事件类型
-            currentTime: new Date().getTime(),                           // 当前时间
-            extraInfo: ''                                                 // 扩展参数
+            userId: utils.cookie.getItem('mall_userId') || '',          // 用户id
+            sessionId: utils.cookie.getItem('sessionId') || '',         // 设备号
+            plantform: '0',                                             // 平台
+            pageUrl: location.href,                                     // 当前页面地址
+            pageName: document.title || '',                             // 当前页面的标题
+            pageFrom: document.referrer,                                // 当前页面的来源
+            eventType: '',                                              // 事件类型
+            currentTime: new Date().getTime(),                          // 当前时间
+            extraInfo: ''                                               // 扩展参数
         }
-        this.intervalInstantiate = null // 定时器的实例
+
+        // 定时器的实例
+        this.intervalInstantiate = null 
+
         // 初始化的时候重置参数
         this._extraData(obj);
         
         // 生成唯一的设备id
         this._createSessionId();
+    
+        //对象初始化时改写history
+        this._pageListener();  
 
     }
     
@@ -71,6 +75,35 @@ class DF_SDK_Collect {
         }
     }
 
+    //改写history
+    _pageListener(){
+        var _wr = function(type) {
+           var orig = history[type];
+           return function() {
+               var rv = orig.apply(this, arguments);
+              var e = new Event(type);
+               e.arguments = arguments;
+               window.dispatchEvent(e);
+               return rv;
+           };
+        };
+        history.pushState = _wr('pushState');
+        history.replaceState = _wr('replaceState');
+    }
+
+    //定时上报
+    _setInterval(cTime){
+        var that = this;
+        var obj = {
+            pageUrl: location.href
+        };
+        that.pageVisit(obj);
+        clearInterval(that.intervalInstantiate);
+        that.intervalInstantiate = setInterval(()=>{
+            that.pageVisit(obj);
+        }, cTime);
+    }
+
     /**
      * PV事件
      * @method pageVisit
@@ -95,7 +128,7 @@ class DF_SDK_Collect {
     }
 
     /**
-     * click 点击事件
+     * 点击事件
      * @method clickHandler (obj)                 
      * @param {String} pageChannel     页面频道
      * @param {String} pagePosition    当前位置    
@@ -108,54 +141,35 @@ class DF_SDK_Collect {
         sendLog(this.sendUrl, obj);
     }
 
-
     /**
-     * @method setIntervalPv 定时上报pv来统计页面的访问时长;
-     * @param cb 回掉函数
-     * @param cTime 频次
+     * 定时上报
+     * @method setIntervalPv 
+     * @param {Number} cTime    时间
      */
-    _setIntervalPv(cb,cTime){
-        if(this.project === 'VUE'){ // 是vue单页面
-            if(this.router){
-                this.router.beforeEach((to, from, next) => {
-                    console.log(this)
-                    if(this.intervalInstantiate){
-                        console.log('hhhhhh')
-                        clearInterval(this.intervalInstantiate);
-                        this.intervalInstantiate = setInterval(()=>{
-                            cb.bind(this)()
-                        }, cTime);
-                    }else{
-                        console.log('ppppp')
-                        this.intervalInstantiate = setInterval(()=>{
-                            cb.bind(this)()
-                        }, cTime);
-                    }
-                    next()
-                })
-            }else{
-                console.log('你好像忘记传路由对象了')
-            }
-           
-        }else{
-            this.intervalInstantiate = setInterval(()=>{
-                cb.bind(this)()
-            }, cTime);
-        }
-    }
-    setIntervalPv(cb,cTime){
-        cTime = cTime ? cTime : 10000;
-        cb = cb ? cb : function(){};
-        this._setIntervalPv(cb,cTime);
+    setIntervalPv(cTime){
+        var that = this;
+        //时间容错
+        if(typeof cTime !== 'number' || isNaN(cTime) || cTime < 1000 ) return;
+        //初始化时
+        that._setInterval(cTime);
+        //导航标签隐藏时
         utils.showState((isVisible)=>{
             if(isVisible === 'visible'){
-                this._setIntervalPv(cb,cTime)
+                that._setInterval(cTime);
             }else{
-                clearInterval(this.intervalInstantiate);
+                clearInterval(that.intervalInstantiate);
             }
         })
+        //spa页面路由变化时
+        window.addEventListener('replaceState', function(e) {
+            that._setInterval(cTime);
+        });
+        window.addEventListener('pushState', function(e) {
+            that._setInterval(cTime);
+        });
     }
 
+    
 }
 
 
